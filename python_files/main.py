af46eff3eaaf084cda6eaa5e00b20eb3a4724275
RedContain <ilyaushakoff1@gmail.com>
2025-10-25 14:34:52 +0300

    Сделал шаблон для главного окна

--- python_files/main.py
+++ python_files/main.py
@@ -1,4 +1,13 @@
-print("Hello World")
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox   # Шаблон полключения главного окна
+from ui_main import Ui_MainWindow
 
-
-print("!")
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
