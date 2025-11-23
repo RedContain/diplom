812251b54f9694430bf2c5b21472422b79901f16
RedContain <ilyaushakoff1@gmail.com>
2025-11-23 18:20:26 +0300

    Сделал поток с таймером

--- python_files/main.py
+++ python_files/main.py
@@ -1,11 +1,30 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PySide6.QtCore import QTimer
+from PySide6.QtWidgets import QApplication, QMainWindow
 from ui_main import Ui_MainWindow
 
-class MainWindow(QMainWindow, Ui_MainWindow):
+class MainWindow(QMainWindow, Ui_MainWindow):                                           # Инициализация класса
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.counter = 0
+        QTimer.singleShot(100, self.start_progress)
+
+    def start_progress(self):                                                           # Функция, которая начинает отчитывать время
+        self.counter = 0
+        self.progressBar.setValue(0)
+
+        self.timer = QTimer()                                                           #Таймер
+        self.timer.timeout.connect(self.update_progress)
+        self.timer.start(30)
+
+    def update_progress(self):                                                          # Функция обновления
+        self.counter += 1
+        self.progressBar.setValue(self.counter)
+
+        if self.counter >= 100:
+            self.timer.stop()
+            self.progressBar.setValue(0)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
