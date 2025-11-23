77a96caa9f2335f5b352137b3666823b10e682be
RedContain <ilyaushakoff1@gmail.com>
2025-11-23 18:54:59 +0300

    Сделал окно отчетности

---
+++ python_files/code/report_window.py
@@ -0,0 +1,16 @@
+import sys
+from PySide6.QtCore import QTimer
+from PySide6.QtWidgets import QMainWindow, QApplication
+
+from python_files.code_ui.ui_reportwindow import Ui_reportWindow
+
+class report_window(QMainWindow, Ui_reportWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = report_window()
+    window.show()
+    sys.exit(app.exec())
