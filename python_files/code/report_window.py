e3b7e22bd8f6e691529d54db4c5a9fce2f4b9337
RedContain <ilyaushakoff1@gmail.com>
2025-12-01 19:03:45 +0300

    Сделал комменты

--- python_files/code/report_window.py
+++ python_files/code/report_window.py
@@ -4,12 +4,12 @@ from PySide6.QtWidgets import QMainWindow, QApplication
 
 from python_files.code_ui.ui_reportwindow import Ui_reportWindow
 
-class report_window(QMainWindow, Ui_reportWindow):
+class report_window(QMainWindow, Ui_reportWindow):                                                                      #Инициализация формы
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
-if __name__ == "__main__":
+if __name__ == "__main__":                                                                                              #Главный класс(точка входа в программу)
     app = QApplication(sys.argv)
     window = report_window()
     window.show()
